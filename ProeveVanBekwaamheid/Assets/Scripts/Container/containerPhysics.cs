8cc21e52e6b7640b351c7a9f500d40ff12f5d32e
Toon <toonschaap3@gmail.com>
2021-03-29 14:00:59 +0200

    Container push 1
    Started on physics script

---
+++ ProeveVanBekwaamheid/Assets/Scripts/Container/containerPhysics.cs
@@ -0,0 +1,28 @@
+﻿//made by toon
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class containerPhysics : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private bool isGrounded;
+
+    void Awake()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+        //leaving these two active will only make pushing the container with the watercannon more frustrating, so they'll always be turned off
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded = false)
+        {
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+}
+
+
