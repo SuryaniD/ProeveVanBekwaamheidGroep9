1dafa34553699c93e4d922f5d54e3acee6cb7485
ClamDaButther <ClamDaButther@gmail.com>
2021-04-01 10:20:39 +0200

    Update1.1
    First push, with minimal codes.

---
+++ ProeveVanBekwaamheid/Assets/Scenes/Score.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public float CurrentScore;
+
+    //Get-set
+    public float RemTime;
+
+    //Get-set 
+    public int EnemiesDead;
+
+    void Start()
+    {
+        
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+}
