f3248dd31232718038d1943de74393a77848e046
ClamDaButther <ClamDaButther@gmail.com>
2021-04-01 11:27:50 +0200

    Update1.2
    It works, But needs to get tested with the other scripts first (that will prob work too)

--- ProeveVanBekwaamheid/Assets/Scenes/Score.cs
+++ ProeveVanBekwaamheid/Assets/Scenes/Score.cs
@@ -1,35 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    //Tim Gijzen
+    //Set this on the UI
+
+    //Set UI text in here 
+    public Text lifeDisplay;
+
+    //The score
     public float CurrentScore;
 
     //Get-set from LevelCountdown Script
     public float RemTime;
 
     //Get-set from EnemyAI Script
-    public int EnemyDead;
+    public float EnemyDead;
 
-    void Start()
-    {
-        
-    }
+    //Set LevelCountdown and EnemyAI on this gameobject to let this script work
+    public GameObject ScriptManager;
 
- 
     void Update()
     {
-        //RemTime = gameObject.GetComponent<LevelCountdown>().RemainingTime;
-        //EnemyDead = gameObject.GetComponent<EnemyAI>().EnemiesDead;
+        //Get values from other scripts script = <>
+        RemTime = ScriptManager.GetComponent<LevelCountdown>().remainingtime;
+        EnemyDead = ScriptManager.GetComponent<EnemyAI>().EnemiesDead;
 
         //RemTime for every second left +10 score
-        
-
         //EnemyDead for every Enemy dead +35 score
+        CurrentScore = RemTime * 10f + EnemyDead * 35f;
 
-
-
-
+        //set score to : Score: CurrentScore
+        lifeDisplay.text = ("Score: ")+CurrentScore.ToString();
     }
 }
