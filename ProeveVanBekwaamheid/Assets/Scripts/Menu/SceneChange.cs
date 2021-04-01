403063f82becc65c48f6597baf3958934d12994c
Toon <toonschaap3@gmail.com>
2021-04-01 12:20:15 +0200

    menu push 1

---
+++ ProeveVanBekwaamheid/Assets/Scripts/Menu/SceneChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public int timer = 1;
+
+    public void Switcher()
+    {
+        timer = timer -= 1;
+        if (timer == 0)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
