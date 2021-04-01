c366fb88500da1dbb7a676385264356e72e56aa9
ClamDaButther <ClamDaButther@gmail.com>
2021-04-01 09:55:43 +0200

    1.3
    Script works, ready to be implemented.

--- ProeveVanBekwaamheid/Assets/Scenes/Tim/RandomLevelGenerator.cs
+++ ProeveVanBekwaamheid/Assets/Scenes/Tim/RandomLevelGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RandomLevelGenerator : MonoBehaviour
 {
+    //Tim Gijzen
+    //MapTile list for premade tiles(specific prebuild mapparts for specific locations)
     public GameObject[] MapTileSet1;
     public GameObject[] MapTileSet2;
     public GameObject[] MapTileSet3;
@@ -14,38 +16,23 @@ public class RandomLevelGenerator : MonoBehaviour
     public GameObject[] MapTileSet8;
     public GameObject[] MapTileSet9;
 
+    //Location where object spawns(placed empty object for vector3)
     public GameObject[] TileLocations;
 
+    //Final Map
     public GameObject MadeMap;
 
     void Start()
-    {
-       
+    {    
+        // Spawns Maptile - select out list random - set on specified location by empty object(placed in game) - adds to mademap as child
         Instantiate(MapTileSet1[Random.Range(1, 4)], TileLocations[1].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
-        // Instantiate(MapTileSet1[Random.Range(1, 4)], TileLocations[1].transform.position, Quaternion.identity, MadeMap.transform);
-        // public static Object Instantiate(Object original, Vector3 position, Quaternion rotation, Transform parent);
         Instantiate(MapTileSet2[Random.Range(1, 4)], TileLocations[2].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
         Instantiate(MapTileSet3[Random.Range(1, 4)], TileLocations[3].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
-        //4
-        //5
-        //6
-        //7
-        //8
-        //9
-
-
-        //Make maptilesets child of MadeMap
-       // MapTileSet1[1].transform.parent = MadeMap.transform;
-
-    }
-
-
-    void Update()
-    {
-       // Debug.Log(Random.Range(1,4));
-
-
-
-
+        Instantiate(MapTileSet4[Random.Range(1, 4)], TileLocations[4].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
+        Instantiate(MapTileSet5[Random.Range(1, 4)], TileLocations[5].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
+        Instantiate(MapTileSet6[Random.Range(1, 4)], TileLocations[6].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
+        Instantiate(MapTileSet7[Random.Range(1, 4)], TileLocations[7].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
+        Instantiate(MapTileSet8[Random.Range(1, 4)], TileLocations[8].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
+        Instantiate(MapTileSet9[Random.Range(1, 4)], TileLocations[9].transform.position, Quaternion.identity).transform.parent = MadeMap.transform;
     }
 }
