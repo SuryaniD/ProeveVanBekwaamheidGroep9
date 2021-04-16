c7c7aa050b091b5c53dda5976b73b86fb66c1de9
Toon <toonschaap3@gmail.com>
2021-04-16 12:38:38 +0200

    waterbom push 5
    finished, only needs effects now

--- ProeveVanBekwaamheid/Assets/Scripts/Waterbom/ExplodingBullet.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Waterbom/ExplodingBullet.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class ExplodingBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject explodableObject;
+    private float entry = 500.0f;
+    private float radius = 50.0f;
+
+    void OnCollisionEnter(Collision collision)
     {
-        
+        //When the bom collides, it creates the explosion effect. Then deletes the bomb and deletes the explosion effect three seconds later
+        //GameObject explodingObject = Instantiate(explodableObject, transform.position, transform.rotation);
+        //Destroy(explodableObject, 3);
+        knockBack();
+       Destroy(gameObject);     
     }
 
-    // Update is called once per frame
-    void Update()
+    void knockBack()
     {
-        
+        //Captures all the nearby colliders using a radius
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObjects in colliders)
+        {
+            Rigidbody rigidbody = nearbyObjects.GetComponent<Rigidbody>();
+            if(rigidbody != null)
+            {
+                //first two elements are looking for where the explosion is going start, the third looks for how big the explosion is going to be
+                rigidbody.AddExplosionForce(entry, transform.position, radius);
+            }
+        }
     }
 }
