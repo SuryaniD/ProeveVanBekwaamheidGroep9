adcbd494c6edcd12a8106bca87650917edec1c99
ClamDaButther <ClamDaButther@gmail.com>
2021-04-22 18:34:09 +0200

    addExtrasScript
    Added Extras script (materials and triggers)

---
+++ ProeveVanBekwaamheid/Assets/Scripts/addExtras.cs
@@ -0,0 +1,41 @@
+﻿//Tim
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addExtras : MonoBehaviour
+{
+    public GameObject[] allContainers;
+    public GameObject[] colliderHoldingList;
+    public GameObject moreCollider;
+    public Material[] addMaterials;
+    public float wFS = 0.3f;
+    public float startWait = 0.4f;
+
+
+    void Start()
+    {
+        waitForSpawn();
+        spawnWaves();
+    }
+
+    IEnumerator waitForSpawn()
+    {
+        yield return new WaitForSeconds(wFS);
+        allContainers = GameObject.FindGameObjectsWithTag("Container");
+    }
+
+    IEnumerator spawnWaves()
+    {
+        
+        yield return new WaitForSeconds(startWait);
+
+        for (int i = 0; i < allContainers.Length; i++)
+        {
+         
+            colliderHoldingList[i] = Instantiate(moreCollider);
+            allContainers[i].transform.parent = colliderHoldingList[i].transform;
+            allContainers[i].GetComponent<MeshRenderer>().material = addMaterials[Random.Range(0, 3)];
+        }
+    }
+}
