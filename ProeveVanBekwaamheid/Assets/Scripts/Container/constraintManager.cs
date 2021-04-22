a3dffb17ecd0aa1571c7edcaa7300c76d6917e6d
Toon <toonschaap3@gmail.com>
2021-04-22 16:53:08 +0200

    crane push 2
    working on adding the option for the player to grab object and let go via input

--- ProeveVanBekwaamheid/Assets/Scripts/Container/constraintManager.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Container/constraintManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //The point of this script is to alter how the container behaves depending on what it collides with.
-public class ConstraintManager : MonoBehaviour
+public class constraintManager : MonoBehaviour
 {
     private Rigidbody rigidBody;
     private bool stackedObject;
@@ -18,40 +18,44 @@ public class ConstraintManager : MonoBehaviour
 
     void OnCollisionEnter(Collision collider)
     {
-       if (collider.gameObject.tag.Equals("Container") == true)
-           
-       {
+        if (collider.gameObject.tag.Equals("Container") == true)
+
+        {
             /*The positions X and Z are disabled so that if the container doesnt perfectly land on the other container, it doesn't automatically fall off. This'll make gameplay and placing 
               containers less frustrating*/
 
             /*the code for rotation constraints is repeated multiple times because whenever a new line is executed where constrains are enabled, it disables the constraints done in a previous
-              line of code.*/  
+              line of code.*/
             rigidBody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
             stackedObject = true;
-       }
+        }
         if (collider.gameObject.tag.Equals("Ground") == true)
         {
             rigidBody.mass = 1000;
-            
+
         }
 
-          if (collider.gameObject.tag.Equals("Ground") && stackedObject == true)
-          {
+        if (collider.gameObject.tag.Equals("Ground") && stackedObject == true)
+        {
 
             //If a container stacked on another container falls to the ground, it'll despawn after 10 seconds
             rigidBody.constraints = RigidbodyConstraints.FreezeRotationX;
             rigidBody.mass = 25;
             Destroy(gameObject, destroyTimer);
-          }
+        }
+
+        if (collider.gameObject.tag.Equals("Crane") == true && Input.GetKey(KeyCode.Space))
+        {
+            this.rigidBody.useGravity = false;
+        }
+
+        void OnCollisionExit(Collision collision)
+        {
+            //when no collision is made, all the other contraints reset, besides the two rotations
+            rigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+        }
 
     }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        //when no collision is made, all the other contraints reset, besides the two rotations
-        rigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
-    }
-
 }
 
 
--- ProeveVanBekwaamheid/Assets/Scripts/crane/stickToObject.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/crane/stickToObject.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class stickToObject : MonoBehaviour
 {
-    Collision col;
 
     void OnCollisionEnter(Collision col)
     {
-        // creates joint
-        FixedJoint joint = gameObject.AddComponent<FixedJoint>();
-        // sets joint position to point of contact
-        joint.anchor = col.contacts[0].point;
-        // conects the joint to the other object
-        joint.connectedBody = col.contacts[0].otherCollider.transform.GetComponentInParent<Rigidbody>();
-        // Stops objects from continuing to collide and creating more joints
-        joint.enableCollision = false;
+     
+        
+            // creates joint
+            FixedJoint joint = gameObject.AddComponent<FixedJoint>();
+            // sets joint position to point of contact
+            joint.anchor = col.contacts[0].point;
+            // conects the joint to the other object
+            joint.connectedBody = col.contacts[0].otherCollider.transform.GetComponentInParent<Rigidbody>();
+            // Stops objects from continuing to collide and creating more joints
+            joint.enableCollision = false;
+          
+        
     }
 
 
