fd8c9b740deb5960595ab7041ce790b34d79e571
Toon <toonschaap3@gmail.com>
2021-04-22 08:58:20 +0200

    crane grab push 1
    object sticks to eachother

---
+++ ProeveVanBekwaamheid/Assets/Scripts/crane/testMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testMove : MonoBehaviour
+{
+    int speed = 10;
+        
+        public void Update()
+        {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+    }
+
