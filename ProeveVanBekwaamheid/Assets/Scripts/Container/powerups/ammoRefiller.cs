797cd71cd994634c3268be4348a590845f4ba25b
Toon <toonschaap3@gmail.com>
2021-05-17 10:33:51 +0200

    ammp container done

--- ProeveVanBekwaamheid/Assets/Scripts/Container/powerups/ammoRefiller.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Container/powerups/ammoRefiller.cs
@@ -5,22 +5,15 @@ using UnityEngine;
 public class ammoRefiller : MonoBehaviour
 {
     private GameObject finder;
-    // Update is called once per frame
+
     void OnCollisionEnter(Collision collider)
     {
         if (collider.gameObject.tag.Equals("Ground") == true)
         {
             GameObject.FindGameObjectWithTag("WaterCanon").GetComponent<PredictedProjectile>().enabled = true;
-            GameObject.FindGameObjectWithTag("WaterCanon").GetComponent<ammoManager>();
-
-            if (ammoManager.ammo == 5)
-            {
-                ammoManager.ammo = ammoManager.ammo + 0;
-            }
-            else
-            {
-                ammoManager.ammo = ammoManager.ammo + 5;
+            ammoManager.ammo = ammoManager.ammo + 5;
+            Destroy(gameObject);
             }
         }
     }
-}
+
--- ProeveVanBekwaamheid/Assets/Scripts/Waterbom/ammoManager.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Waterbom/ammoManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class ammoManager : MonoBehaviour
 {
-   public int ammo = 5;
+   public static int ammo = 5;
 
     // Update is called once per frame
     void Update()
