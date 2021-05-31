ef9963ad0abd1ad07f8e0bcbb0fb032fb6de81ed
Dexter <31446927+itsdexter66@users.noreply.github.com>
2021-05-31 13:49:20 +0200

    Changes aan de controls
    Zorgt ervoor dat je kan switchen tussen het bedienen van de kraan en van de water canon

---
+++ ProeveVanBekwaamheid/Assets/ControlsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsManager : MonoBehaviour
+{
+    public WaterCanon waterCanon;
+    public craneMovement CraneMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            if (CraneMovement.craneControlsActive == false)
+            {
+                CraneMovement.craneControlsActive = true;
+                waterCanon.waterControlsActive = false;
+            }
+
+            else if (CraneMovement.craneControlsActive == true)
+            {
+                CraneMovement.craneControlsActive = false;
+                waterCanon.waterControlsActive = true;
+            }
+        }
+    }
+}
