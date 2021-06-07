a1ad828f49c73cadc5b4ff58d03a261b3b001c09
Toon <toonschaap3@gmail.com>
2021-06-07 14:15:56 +0200

    story scene added

---
+++ ProeveVanBekwaamheid/Assets/Scripts/gotoInstructions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gotoInstructions : MonoBehaviour
+{
+        public void NextScene()
+        {
+            SceneManager.LoadScene("Instructions");
+        }
+    }
+
