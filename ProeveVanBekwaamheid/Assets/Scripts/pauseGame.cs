b74318aa5a10d3e8e66368b40f988408dcf9c08c
Toon <toonschaap3@gmail.com>
2021-06-07 11:59:35 +0200

    added pause function and updated instruction screen

---
+++ ProeveVanBekwaamheid/Assets/Scripts/pauseGame.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pauseGame : MonoBehaviour
+{
+    bool isPaused;
+    public Image pauseScreen;
+
+    void Start()
+    {
+        isPaused = false;   
+    }
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+           
+            if (!isPaused)
+            {
+                Pause();
+             
+            }
+            else Unpause();
+               
+        }
+       
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+        var opacity = pauseScreen.color;
+        opacity.a = 1f;
+        pauseScreen.color = opacity;
+    }
+
+    void Unpause()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+        var opacity = pauseScreen.color;
+        opacity.a = 0f;
+        pauseScreen.color = opacity;
+    }
+}
