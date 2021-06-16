07b9f15ae7468be4583a2ad47698bc5a03883601
ClamDaButther <ClamDaButther@gmail.com>
2021-06-16 02:00:04 +0200

    Update HP & FIGHT
    Added Dmg and HP,
    Added That enemy's fight back,
    Added u can die and enemy can die,
    Added That u can fight others enemy after enemy defeated.

---
+++ ProeveVanBekwaamheid/Assets/Scripts/Dobbel/DiceFight.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceFight : MonoBehaviour
+{
+
+    public DobbelManager dobbelManager;
+    [SerializeField]
+    private GameObject[] enemyPiratesArray;
+
+    private GameObject playerChar;
+
+    private int newDmgPlayer;
+    private int oldDmgPlayer;
+    private int newDmgEnemy;
+    private int oldDmgEnemy;
+
+    private int playerHealth = 50;
+    private int enemyHealth = 6;
+
+    private int hasThrown = 0;
+    private int hasThrownEnemy = 0;
+
+    public int piratesDestroyed = 0;
+
+
+    void Start()
+    {
+        enemyPiratesArray = dobbelManager.enemyPirate;
+        playerChar = dobbelManager.questPlayer;
+    }
+    
+
+    void Update()
+    {
+        newDmgPlayer = dobbelManager.hitDmg;
+        newDmgEnemy = dobbelManager.enemyHitDmg;
+
+        if (oldDmgPlayer != newDmgPlayer)
+        {
+            hasThrown++;
+            oldDmgPlayer = newDmgPlayer;
+        }
+
+
+        if(oldDmgEnemy != newDmgEnemy)
+        {
+            hasThrownEnemy++;
+            oldDmgEnemy = newDmgEnemy;
+        }
+
+
+        if(hasThrown >= 1)
+        {
+            hasThrown--;
+
+            enemyHealth = enemyHealth - oldDmgPlayer;
+            Debug.Log(hasThrown);
+        }
+
+        if (hasThrownEnemy >= 1)
+        {
+            hasThrownEnemy--;
+
+            playerHealth = playerHealth - oldDmgEnemy;
+            Debug.Log(hasThrownEnemy);
+        }
+
+        if (playerHealth <= 0)
+        {
+            Destroy(playerChar);
+        }
+
+        if(enemyHealth <= 0)
+        {
+            enemyHealth = 6;
+            Destroy(enemyPiratesArray[piratesDestroyed]);
+            piratesDestroyed++;
+        }
+    }
+}
--- ProeveVanBekwaamheid/Assets/Scripts/Dobbel/DobbelManager.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/Dobbel/DobbelManager.cs
@@ -4,38 +4,77 @@ using UnityEngine;
 
 public class DobbelManager : MonoBehaviour
 {
+    public Score score;
+    public DiceFight diceFight;
+
+    public GameObject[] enemyPirate;
+
     [SerializeField]
     private GameObject prefabDice;
     private GameObject dice;
 
-    [SerializeField]
-    private GameObject questPlayer;
+    
+    public GameObject questPlayer;
     private Rigidbody rb;
 
     private bool rollBool1 = false;
     private bool rollBool2 = false;
     private bool rollingDiceBool = true;
 
-    private int hitDmg;
+    private bool enemyIsPlaying = false;
+    private bool antiRepeatEP = false;
+
+    public int timesRolled;
+
+    public int hitDmg;
+    public int enemyHitDmg;
+
+    private int newEnemysDeadScore = 0;
+    private int oldEnemysDeadScore = 0;
+    private int throwScore = 0;
+
+    private int whichPirate = 0;
+
+    private int newSQPirateKills;
+    private int oldSQPirateKills;
 
     void Start()
     {
+        
         rb = questPlayer.GetComponent<Rigidbody>();
     }
     
     void Update()
     {
+        //NEDS is always the same as enemyDead score from Score script
+        //checks if newscore changed in value then gives throwScore +1 for how manny times u can use the dice
+        //(U can use the dice as many times as u splett a pirate)
+        newEnemysDeadScore = score.enemyDead;
+        newSQPirateKills = diceFight.piratesDestroyed;
+
+        if (oldEnemysDeadScore != newEnemysDeadScore)
+        {
+            throwScore++;
+            oldEnemysDeadScore = newEnemysDeadScore;
+        }
+
+        if (oldSQPirateKills != newSQPirateKills)
+        {
+            whichPirate++;
+            oldSQPirateKills = newSQPirateKills;
+        }
+
         //If get input Up spawn and start rolling dice
-        if (Input.GetKeyDown(KeyCode.UpArrow) && rollBool1 == false)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && rollBool1 == false && throwScore >= 1 && enemyIsPlaying == false)
         {
             rollBool1 = true;
+            throwScore--;
             Instantiate(prefabDice, new Vector3(questPlayer.transform.position.x, questPlayer.transform.position.y + 4,questPlayer.transform.position.z), Quaternion.identity);
             rollingDiceBool = true;
             dice = GameObject.FindGameObjectWithTag("DobbelSteen");
             StartCoroutine(RollingDice());
             StartCoroutine(RrollBool2SetTrue());
         }
-
 
         //If get input Up again Jump and get number on dice
         if (Input.GetKeyDown(KeyCode.UpArrow) && rollBool2 == true)
@@ -44,9 +83,15 @@ public class DobbelManager : MonoBehaviour
             rollingDiceBool = false;
             rb.AddForce(Vector3.up * 500);
             hitDmg = Random.Range(1,7);
-            Debug.Log(hitDmg); 
             StartCoroutine(RotatingDice());
         }
+
+        if (enemyIsPlaying == true && antiRepeatEP == false)
+        {
+            antiRepeatEP = true;
+            StartCoroutine(LetEnemyPlay());
+        }
+
     }
 
     //Moet paar seconden wachten omdat hij anders meteen de 2e input meeneemt
@@ -107,34 +152,75 @@ public class DobbelManager : MonoBehaviour
     IEnumerator RotatingDice()
     {
         yield return new WaitForSeconds(0.1f);
-        if (hitDmg == 1)
+        if (hitDmg == 1 || enemyHitDmg == 1)
         {
             dice.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
         }
-        else if(hitDmg == 2)
+        else if(hitDmg == 2 || enemyHitDmg == 2)
         {
             dice.transform.rotation = Quaternion.Euler(-90.0f, 0.0f, 0.0f);
         }
-        else if (hitDmg == 3)
+        else if (hitDmg == 3 || enemyHitDmg == 3)
         {
             dice.transform.rotation = Quaternion.Euler(180.0f, 90.0f, 90.0f);
         }
-        else if (hitDmg == 4)
+        else if (hitDmg == 4 || enemyHitDmg == 4)
         {
             dice.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
         }
-        else if (hitDmg == 5)
+        else if (hitDmg == 5 || enemyHitDmg == 5)
         {
             dice.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
         }
-        else if (hitDmg == 6)
+        else if (hitDmg == 6 || enemyHitDmg == 6)
         {
             dice.transform.rotation = Quaternion.Euler(0.0f, 90.0f, 180.0f);
         }
 
+        if (enemyIsPlaying == false)
+        {
+            enemyIsPlaying = true;
+            yield return new WaitForSeconds(2);
+            Destroy(dice.gameObject);
+            timesRolled++;
+            rollBool1 = false;
+        }
+    }
+
+
+    IEnumerator LetEnemyPlay()
+    {
+        yield return new WaitForSeconds(5);
+        Instantiate(prefabDice, new Vector3(enemyPirate[whichPirate].transform.position.x, enemyPirate[whichPirate].transform.position.y + 4, enemyPirate[whichPirate].transform.position.z), Quaternion.identity);
+        dice = GameObject.FindGameObjectWithTag("DobbelSteen");
+
+        yield return new WaitForSeconds(0.5f);
+
+        dice.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        yield return new WaitForSeconds(0.2f);
+  
+        dice.transform.rotation = Quaternion.Euler(-90.0f, 0.0f, 0.0f);
+        yield return new WaitForSeconds(0.2f);
+
+        dice.transform.rotation = Quaternion.Euler(180.0f, 90.0f, 90.0f);
+        yield return new WaitForSeconds(0.2f);
+     
+        dice.transform.rotation = Quaternion.Euler(90.0f, 0.0f, 0.0f);
+        yield return new WaitForSeconds(0.2f);
+      
+        dice.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+        yield return new WaitForSeconds(0.2f);
+        
+       
+        dice.transform.rotation = Quaternion.Euler(0.0f, 90.0f, 180.0f);
+        yield return new WaitForSeconds(0.2f);
+
+        enemyHitDmg = Random.Range(1, 7);
+        enemyPirate[whichPirate].GetComponent<Rigidbody>().AddForce(Vector3.up * 500); ;
+        StartCoroutine(RotatingDice());
         yield return new WaitForSeconds(2);
         Destroy(dice.gameObject);
-
-        rollBool1 = false;
+        antiRepeatEP = false;
+        enemyIsPlaying = false;
     }
 }
