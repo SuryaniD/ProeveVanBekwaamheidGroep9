32a84addb8f1ed79ddb6e4f6f860063ac5ddb403
Toon <toonschaap3@gmail.com>
2021-06-16 10:05:31 +0200

    main menu doesnt change scene when any button is pressed anymore

--- ProeveVanBekwaamheid/Assets/Scripts/MainMenuScript.cs
+++ ProeveVanBekwaamheid/Assets/Scripts/MainMenuScript.cs
@@ -6,15 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    
+
     void Update()
     {
         //rotates on Y
         transform.Rotate(0, 0.013f, 0 * Time.deltaTime);
         //Presses any key to change scene
-        if (Input.anyKey)
-        {
-           SceneManager.LoadScene (sceneName: "story");
-        }
+
     }
 }
