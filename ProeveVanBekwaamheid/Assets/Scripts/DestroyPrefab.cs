43b7bf82c3f3baca5c2c14e4548030f113141a60
SuryaniD <bytebytermail@gmail.com>
2021-06-17 11:17:38 +0200

    Added waterimpact prefab

---
+++ ProeveVanBekwaamheid/Assets/Scripts/DestroyPrefab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPrefab : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this.gameObject, 0.5f);
+    }
+}
